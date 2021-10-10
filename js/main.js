61d5fd7041b053e7614927b15d444258504ce664
TrendonRobinson <Trendonrobinson@gmail.com>
2021-10-09 21:44:22 -0400

    commit

--- js/main.js
+++ js/main.js
@@ -1,20 +1,35 @@
-$.ajax({
-    method: 'get',
-    url: 'https://dog.ceo/api/breeds/list/all'
-})
-// success? add li for each breed to the ul
-.then(response => {
+console.log($)
+// $.ajax({
+//     method: 'get',
+//     url: 'https://maps.googleapis.com/maps/api/distancematrix/json'
+// })
+// // success? add li for each breed to the ul
+// .then(response => {
     
-    let dogsObjects = response.message
+//     let dogsObjects = response.message
     
-    let listHtml = ''
-    for (const key in dogsObjects){
-        // $('ul').append($(`<li>${key}<li>`))
+//     let listHtml = ''
+//     for (const key in dogsObjects){
+//         // $('ul').append($(`<li>${key}<li>`))
 
-        listHtml +=`<li>${key}<li>`
-    }
+//         listHtml +=`<li>${key}<li>`
+//     }
 
-    $('ul').append(listHtml)
-})
-// failed? log error message
-.catch(() => console.log('error'))
+//     $('ul').append(listHtml)
+// })
+// // failed? log error message
+// .catch(() => console.log('error'))
+
+// function initMap() {
+
+//     console.log("test")
+
+//     map = new google.maps.Map(document.getElementById("map"), {
+//       center: { lat: -34.397, lng: 150.644 },
+//       zoom: 8,
+//     });
+
+//     console.log(map)
+// }
+
+// initMap()
