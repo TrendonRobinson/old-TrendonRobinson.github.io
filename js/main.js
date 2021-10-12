bd6a2bdfc3e5a4c6edeb3f8b7b0693253720ec5a
TrendonRobinson <Trendonrobinson@gmail.com>
2021-10-11 20:20:44 -0400

    Final Commit for now

--- js/main.js
+++ js/main.js
@@ -15,29 +15,46 @@ function findClosing(arr, i) {
 for (let index = 0; index < animatedTitles.length; index++) {
     const element = animatedTitles[index];
     // console.log([...element.innerHTML])
-    let arr = [...element.innerHTML]
-        let string = ""
-        let getFullTag = ""
-        let ignoreText = false
+    let arr = [...element.innerHTML];
+    let string = ""
+    let getFullTag = ""
+    let ignoreText = false
         
-        for (let i = 0; i < arr.length; i++) {
-            const letter = arr[i];
-            if (arr[i] == "<") {
-                ignoreText = true
-                getFullTag = findClosing(arr, i)
-                string += `${getFullTag}`
-                getFullTag = ""
-            }
-
-            if (!ignoreText) {
-                string += `<span class="letter-highlight">${letter}</span>`
-            }
-
-            if (arr[i] == ">") {
-                ignoreText = false
-            }
-
+    for (let i = 0; i < arr.length; i++) {
+        const letter = arr[i];
+        if (arr[i] == "<") {
+            ignoreText = true
+            getFullTag = findClosing(arr, i)
+            string += `${getFullTag}`
+            getFullTag = ""
         }
-        element.innerHTML = string
+
+        if (!ignoreText) {
+            string += `<span class="letter-highlight">${letter}</span>`
+        }
+
+        if (arr[i] == ">") {
+            ignoreText = false
+        }
+
+    }
+    element.innerHTML = string;
 }
 
+let hamNav = $('#hamburger-nav');
+let hamNavItems = $('#hamburger-nav-items');
+
+console.log(hamNav);
+
+let hamExpand = false;
+
+hamNav.click(function(element){
+    if (hamExpand) {
+        hamNavItems.css({"display": "none"});
+        hamExpand = false;
+    } else {
+        hamNavItems.css({"display": "flex"})
+        hamExpand = true;
+    }
+})
+console.log("test")
